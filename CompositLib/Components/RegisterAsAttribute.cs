0ffe54238dd2766891531e54a765a0826cf0a834
Lua-cat <153029535+Lua-cat@users.noreply.github.com>
2026-04-27 14:09:17 +0200

    Version 1.0

---
+++ CompositLib/Components/RegisterAsAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace CompositLib.Components;
+
+using System;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+public sealed class RegisterAsAttribute : Attribute
+{
+  public Type Type { get; }
+
+  public RegisterAsAttribute(Type type)
+  {
+    Type = type;
+  }
+}
