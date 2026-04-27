00d3638468b4e50c684ed84bd0f3e2d9d1e4fc86
Lua-cat <153029535+Lua-cat@users.noreply.github.com>
2026-04-27 15:01:25 +0200

    test

--- CompositLib/.godot/mono/temp/obj/Debug/CompositLib.AssemblyInfo.cs
+++ CompositLib/.godot/mono/temp/obj/Debug/CompositLib.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("A Library for doing component based code")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b1e1a9f2f1f6059f5b4dab9664f6408237942e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompositLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompositLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
